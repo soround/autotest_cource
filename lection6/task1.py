c3b72697832b69547c8dbb7d0be5cf1b3b094786
Ivan <ivan007244@gmail.com>
2023-05-17 19:59:32 +0700

    task 1-4 cleanup

---
+++ lection6/task1.py
@@ -0,0 +1,29 @@
+# Глобальные перемены.
+# Имеется следующие переменные, определенные в глобальной области видимости модуля:
+# number = 1
+# string = 'Hello'
+# Напишите и вызовите функцию, которая будет изменять и возвращать эти переменные, на следующие значения:
+# number = 5
+# string = 'Hello, dear friend'
+# 
+
+number = 1
+string = 'Hello'
+
+
+def global_changes():
+    # Здесь нужно написать код
+    globals()['number'] = 5
+    globals()['string'] = 'Hello, dear friend'
+
+    return globals()['number'], globals()['string']
+
+
+global_changes()
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+assert number == 5, 'Переменная number должна иметь значение 5'
+assert string == 'Hello, dear friend', 'Переменная number должна иметь значение Home, sweet home'
+assert global_changes() == (5, 'Hello, dear friend')
+
+print('Все ок')
