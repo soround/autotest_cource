999ca36d75a2bc5de38da58b1896d4019aca18fe
ivan <ivan007244@gmail.com>
2023-06-08 00:53:46 +0700

    solution all tasks

--- lection10/task3/test_case.py
+++ lection10/task3/test_case.py
@@ -11,10 +11,10 @@ def all_division(*arg1):
     return division
 
 
-@pytest.mark.parametrize("numbers, expected_result",
-                         [((10, 2), 5),
-                          ((20, 4), 5),
-                          pytest.param((30, 0), None, marks=pytest.mark.skip(reason="Division by zero"))],
+@pytest.mark.parametrize("numbers, expected_result", [
+    ((10, 2), 5),
+    pytest.param((20, 4), 5, marks=pytest.mark.smoke),
+    pytest.param((30, 0), None, marks=pytest.mark.skip(reason="Division by zero"))],
                          ids=["Test 1", "Test 2", "Test 3 (skipped)"])
 def test_all_division(numbers, expected_result):
     result = all_division(*numbers)
